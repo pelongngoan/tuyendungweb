d266ac79a18fa5ba9b366ddad14b594db79e78d2
pelongngoan <long.tl446@gmail.com>
2024-12-07 03:48:04 +0700

    [BE][Feature]Hot fix

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,35 +1,14 @@
 import express from "express";
-import dotenv from "dotenv";
-import bodyParser from "body-parser";
-import mongoSanitize from "express-mongo-sanitize";
 import dbConnection from "./config/config";
 import router from "./routes";
-import errorMiddleware from "./middlewares/error";
-import cors from "cors";
-dotenv.config();
 
 const app = express();
 
-const PORT = process.env.PORT || 8800;
-
-// MONGODB CONNECTION
+// Connect to the database
 dbConnection();
 
-// middlenames
-app.use(cors());
-app.use(mongoSanitize());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ extended: true }));
-
+// Middleware
+app.use(express.json());
 app.use(router);
 
-//error middleware
-app.use(errorMiddleware);
-app.get("/", (req, res) => {
-  res.json({ message: "Hello from API" });
-});
-app.listen(PORT, () => {
-  console.log(`Dev Server running on port: ${PORT}`);
-});
+export default app;
