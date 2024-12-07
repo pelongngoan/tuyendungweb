58571a6f88e317bf442ff037d2fa8a98cbe02603
pelongngoan <long.tl446@gmail.com>
2024-12-07 10:49:25 +0700

    [BE][Feature]Add vercel

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,14 +1,35 @@
 import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import mongoSanitize from "express-mongo-sanitize";
 import dbConnection from "./config/config";
 import router from "./routes";
+import errorMiddleware from "./middlewares/error";
+import cors from "cors";
+dotenv.config();
 
 const app = express();
 
-// Connect to the database
+const PORT = process.env.PORT || 8800;
+
+// MONGODB CONNECTION
 dbConnection();
 
-// Middleware
-app.use(express.json());
+// middlenames
+app.use(cors());
+app.use(mongoSanitize());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true }));
+
 app.use(router);
 
-export default app;
+//error middleware
+app.use(errorMiddleware);
+app.get("/", (req, res) => {
+  res.json({ message: "Hello from API" });
+});
+app.listen(PORT, () => {
+  console.log(`Dev Server running on port: ${PORT}`);
+});
