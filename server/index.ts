e87ff49e50a51d7a50c19a13471886d139fcd899
pelongngoan <long.tl446@gmail.com>
2024-12-07 14:31:53 +0700

    [BE][Feature]Hot fix vercel for deploy 13

--- server/index.ts
+++ server/index.ts
@@ -1,25 +1,26 @@
 import express from "express";
 import dotenv from "dotenv";
-import bodyParser from "body-parser";
-import mongoSanitize from "express-mongo-sanitize";
 import cors from "cors";
 import dbConnection from "config/config";
 import router from "routes";
 import errorMiddleware from "middlewares/error";
 
 dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 8800;
+// app.use("/", (req, res) => {
+//   res.send("Server is running");
+// });
+app.use(mongoSanitize());
 dbConnection();
 app.use(cors());
-// app.use(mongoSanitize());
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(express.json({ limit: "10mb" }));
-// app.use(express.urlencoded({ extended: true }));
 app.use(router);
 app.use(errorMiddleware);
 app.listen(PORT, () => {
   console.log(`Dev Server running on port: ${PORT}`);
 });
 export default app;
+function mongoSanitize(): any {
+  throw new Error("Function not implemented.");
+}
--- server/public/index.js
+++ server/public/index.js
@@ -12,13 +12,11 @@ const error_1 = __importDefault(require("middlewares/error"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 8800;
+// app.use("/", (req, res) => {
+//   res.send("Server is running");
+// });
 (0, config_1.default)();
 app.use((0, cors_1.default)());
-// app.use(mongoSanitize());
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(express.json({ limit: "10mb" }));
-// app.use(express.urlencoded({ extended: true }));
 app.use(routes_1.default);
 app.use(error_1.default);
 app.listen(PORT, () => {
