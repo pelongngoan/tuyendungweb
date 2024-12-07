1aac9765a105dafea7c8faca1b22fa5d0bcd7d39
pelongngoan <long.tl446@gmail.com>
2024-12-07 14:24:21 +0700

    [BE][Feature]Hot fix vercel for deploy 10

--- server/index.ts
+++ server/index.ts
@@ -9,28 +9,16 @@ import errorMiddleware from "middlewares/error";
 
 dotenv.config();
 const app = express();
-
 const PORT = process.env.PORT || 8800;
-
-// MONGODB CONNECTION
 dbConnection();
-
-// middlenames
 app.use(cors());
 app.use(mongoSanitize());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
-
 app.use(router);
-app.get("/", (req, res) => {
-  res.json({ message: "Hello from API" });
-});
-//error middleware
 app.use(errorMiddleware);
-// Move the root route here
-
 app.listen(PORT, () => {
   console.log(`Dev Server running on port: ${PORT}`);
 });
