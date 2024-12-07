b5b78e9160c1014a4809df3e100877f361bf7080
pelongngoan <long.tl446@gmail.com>
2024-12-07 14:26:30 +0700

    [BE][Feature]Hot fix vercel for deploy 11

--- server/index.ts
+++ server/index.ts
@@ -12,11 +12,11 @@ const app = express();
 const PORT = process.env.PORT || 8800;
 dbConnection();
 app.use(cors());
-app.use(mongoSanitize());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ extended: true }));
+// app.use(mongoSanitize());
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.json({ limit: "10mb" }));
+// app.use(express.urlencoded({ extended: true }));
 app.use(router);
 app.use(errorMiddleware);
 app.listen(PORT, () => {
