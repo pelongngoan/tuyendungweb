c0e577b283981ee9b4a46f06ffb7fe960a306eed
pelongngoan <long.tl446@gmail.com>
2024-12-07 14:27:53 +0700

    [BE][Feature]Hot fix vercel for deploy 12

--- server/public/index.js
+++ server/public/index.js
@@ -5,26 +5,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
-const body_parser_1 = __importDefault(require("body-parser"));
-const express_mongo_sanitize_1 = __importDefault(require("express-mongo-sanitize"));
 const cors_1 = __importDefault(require("cors"));
-const config_1 = __importDefault(require("./config/config"));
-const error_1 = __importDefault(require("./middlewares/error"));
-const routes_1 = __importDefault(require("./routes"));
+const config_1 = __importDefault(require("config/config"));
+const routes_1 = __importDefault(require("routes"));
+const error_1 = __importDefault(require("middlewares/error"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 8800;
-// MONGODB CONNECTION
 (0, config_1.default)();
-// middlenames
 app.use((0, cors_1.default)());
-app.use((0, express_mongo_sanitize_1.default)());
-app.use(body_parser_1.default.json());
-app.use(body_parser_1.default.urlencoded({ extended: true }));
-app.use(express_1.default.json({ limit: "10mb" }));
-app.use(express_1.default.urlencoded({ extended: true }));
+// app.use(mongoSanitize());
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.json({ limit: "10mb" }));
+// app.use(express.urlencoded({ extended: true }));
 app.use(routes_1.default);
-//error middleware
 app.use(error_1.default);
 app.listen(PORT, () => {
     console.log(`Dev Server running on port: ${PORT}`);
